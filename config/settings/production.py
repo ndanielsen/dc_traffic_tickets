701de002bb32e2cc561587fc7bb13e1aa7ee36bc
Nathan <ndanielsen@users.noreply.github.com>
2016-09-19 21:31:12 -0400

    updated permissions (#30)
    * fixed management command and change admin dataset view
    
    * added sql-explored and changed permissions for api and sql explorer view
    
    * added whitelist and blacklist for sql explorer
    
    * added migration;
    
    * updated import command
    
    * updated permission on prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -234,3 +234,8 @@ REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
     'rest_framework.authentication.TokenAuthentication',
     'rest_framework.authentication.SessionAuthentication',
     )
+
+REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
