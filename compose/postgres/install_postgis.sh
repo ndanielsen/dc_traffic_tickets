4670805cc3faa9451bd353ac6f78430cf64b1857
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-12-07 19:59:18 -0500

    fix postgres version

--- compose/postgres/install_postgis.sh
+++ compose/postgres/install_postgis.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install -y --no-install-recommends postgresql-9.5-postgis-2.2 postgis=2.2.2+dfsg-1~bpo8+1
+sudo apt-get install -y --no-install-recommends postgresql-9.5-postgis-2.3 postgis=2.3.1+dfsg-1~bpo8+1
 
 sudo service postgresql restart
